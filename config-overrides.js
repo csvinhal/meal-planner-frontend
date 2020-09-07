2bf1f4db2498b819a108315c63fca0cd588033f4
Cristiano Vinhal <cristiano.vinhal10@gmail.com>
2020-09-07 16:26:21 -0300

    start with typescript and config paths

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,10 @@
 const { useBabelRc, override } = require("customize-cra");
+const { alias, configPaths } = require("react-app-rewire-alias");
 
-module.exports = override(useBabelRc());
+function aliasOverride(config) {
+  alias({ ...configPaths("tsconfig.paths.json") })(config);
+  return config;
+}
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+module.exports = override(useBabelRc(), aliasOverride);
