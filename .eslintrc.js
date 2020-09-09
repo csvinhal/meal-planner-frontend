3fb9a8ea74afec4f1b7b843818775ae4e2271a81
Cristiano Vinhal <cristiano.vinhal10@gmail.com>
2020-09-08 22:38:19 -0300

    system config's

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "react-hooks"
-    ],
-    "rules": {
-        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-        "react-hooks/exhaustive-deps": "warn"  // Checks effect dependencies
-    },
-    "settings": {
-        "react": {
-            "createClass": "createReactClass", // Regex for Component Factory to use,
-                                                // default to "createReactClass"
-            "pragma": "React",  // Pragma to use, default to "React"
-            "version": "detect", // React version. "detect" automatically picks the version you have installed.
-                                // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-                                // default to latest and warns if missing
-                                // It will default to "detect" in the future
-            "flowVersion": "0.53" // Flow version
-        },
-        "propWrapperFunctions": [
-            // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-            "forbidExtraProps",
-            {"property": "freeze", "object": "Object"},
-            {"property": "myFavoriteWrapper"}
-        ],
-        "linkComponents": [
-            // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-            "Hyperlink",
-            {"name": "Link", "linkAttribute": "to"}
-        ]
-    }
-};
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ]
+  },
+}
